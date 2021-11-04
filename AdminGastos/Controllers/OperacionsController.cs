978ef7087809e3dadc9f59561b6df682211f2aa2
djseeba173 <djseeba173@gmail.com>
2021-11-04 16:45:52 -0300

    conceptos funcionales

--- AdminGastos/Controllers/OperacionsController.cs
+++ AdminGastos/Controllers/OperacionsController.cs
@@ -53,7 +53,7 @@ namespace AdminGastos.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("IdOperacion,importe,fecha,producto")] Operacion operacion)
+        public async Task<IActionResult> Create([Bind("IdOperacion,importe,Producto,Concepto,Fecha")] Operacion operacion)
         {
             if (ModelState.IsValid)
             {
@@ -85,7 +85,7 @@ namespace AdminGastos.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("IdOperacion,importe")] Operacion operacion)
+        public async Task<IActionResult> Edit(int id, [Bind("IdOperacion,importe,Producto,Concepto,Fecha")] Operacion operacion)
         {
             if (id != operacion.IdOperacion)
             {
---
+++ AdminGastos/Migrations/20211104191426_seba.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AdminGastos.Migrations
+{
+    public partial class seba : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Concepto",
+                table: "Operacion",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Concepto",
+                table: "Operacion");
+        }
+    }
+}
